594cdabd596ea79b7abb6b3414ab7ccca77b5ca9
Badr Choubai <badrchoubai@outlook.com>
2020-07-25 16:32:49 -0600

    Switch to using lit-element

--- TodoApp.js
+++ TodoApp.js
@@ -1,25 +1,68 @@
+import { html, render } from "https://unpkg.com/lit-html?module";
+import "./TodoItem.js";
+
 const template = document.createElement("template");
 template.innerHTML = `
-    <style>
+`;
+
+class TodoApp extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+
+    this.todos = [
+      { text: "Learn Web Components", checked: true },
+      { text: "Go for a hike", checked: false },
+    ];
+
+    render(this.template(), this._shadowRoot, { eventContext: this });
+
+    this.$input = this._shadowRoot.querySelector("input");
+  }
+
+  _addTodoItem(event) {
+    event.preventDefault();
+
+    if (this.$input.value.length > 0) {
+      this.todos = [...todos, { text: this.$input.value, checked: false }];
+      this.$input.value = ``;
+    }
+  }
+
+  _removeTodoItem(event) {
+    this.todos = this.todos.filter((todo, index) => {
+      return index !== event.detail;
+    });
+  }
+
+  _toggleTodoItem(event) {
+    this.todos = this.todos.map((todo, index) => {
+      return index === event.detail ? { ...todo, check: !todo.checked } : todo;
+    });
+  }
+
+  template() {
+    return html`
+      <style>
         :host {
-            display: block;
-            text-align: left;
+          display: block;
+          text-align: left;
         }
 
         button {
-            border: none;
-            cursor: pointer;
-            padding: 6px 16px;
+          border: none;
+          cursor: pointer;
+          padding: 6px 16px;
         }
 
         ul {
-            align-items: flex-start;
-            display: flex;
-            flex-wrap: wrap;
-            flex-direction: column;
-            justify-content: space-around;
-            list-style: none;
-            padding: 0;
+          align-items: flex-start;
+          display: flex;
+          flex-wrap: wrap;
+          flex-direction: column;
+          justify-content: space-around;
+          list-style: none;
+          padding: 0;
         }
 
         h1 {
@@ -34,9 +77,13 @@ template.innerHTML = `
           padding: 0 16px;
         }
 
+        #todo-input {
+          margin-bottom: 1rem;
+        }
+
         #todos-container {
           background: #f5f5f3;
-          box-shadow: 0 .2em .3em rgba(0, 0, 0, 0.06);
+          box-shadow: 0 0.2em 0.3em rgba(0, 0, 0, 0.06);
           display: flex;
           flex-direction: row;
           flex-wrap: wrap;
@@ -44,45 +91,32 @@ template.innerHTML = `
           padding: 16px;
           justify-content: flex-start;
         }
-    </style>
+      </style>
 
-    <section id="app">
+      <section id="app">
+      <form id="todo-input">
+        <input type="text" placeholder="Add a new to-do"></input>
+        <button @click=${this._addTodoItem}>Add Todo</button>
+      </form>
+
       <h1>Your Todos</h1>
 
-      <div id="todo-input">
-        <input type="text" placeholder="Add a new Todo Item" />
-        <button>Add</button>
-      </div>
-
-      <ul id="todos-container"></ul>
-    </section>
-`;
-
-class TodoApp extends HTMLElement {
-  constructor() {
-    super();
-    this._shadowRoot = this.attachShadow({ mode: "open" });
-    this._shadowRoot.appendChild(template.content.cloneNode(true));
-    this._todos;
-
-    // Element refs
-    this.$todoList = this._shadowRoot.querySelector("ul");
-    this.$input = this._shadowRoot.querySelector("#todo-input").children[0];
-    this.$submitButton = this._shadowRoot.querySelector(
-      "#todo-input"
-    ).children[1];
-
-    this.$submitButton.addEventListener("click", this._addTodoItem.bind(this));
-  }
-
-  connectedCallback() {
-    if (!this._todos) {
-      let $message = document.createElement("p");
-      $message.innerHTML = "No Todos";
-      this.$todoList.appendChild($message);
-    } else {
-      this._renderTodoList();
-    }
+      <ul id="todos-container">
+        ${this.todos.map(
+          (todo, index) => html`
+            <wc-todo-item
+              ?checked=${todo.checked}
+              .index=${index}
+              text=${todo.text}
+              @onRemove=${this._removeTodoItem}
+              @onToggle=${this._toggleTodoItem}
+            >
+            </wc-todo-item>
+          `
+        )}
+      </ul>
+      </section>
+    `;
   }
 
   get todos() {
@@ -91,38 +125,7 @@ class TodoApp extends HTMLElement {
 
   set todos(value) {
     this._todos = value;
-    this._renderTodoList();
-  }
-
-  _addTodoItem() {
-    if (this.$input.value.length > 0) {
-      this._todos.push({ text: this.$input.value, checked: false });
-      this._renderTodoList();
-      this.$input.value = "";
-    }
-  }
-
-  _removeTodoItem(event) {
-    this._todos.splice(event.detail, 1);
-    this._renderTodoList();
-  }
-
-  _renderTodoList() {
-    this.$todoList.innerHTML = ``;
-
-    this._todos.forEach((todo, index) => {
-      let $todoItem = document.createElement("wc-todo-item");
-      $todoItem.setAttribute("text", todo.text);
-
-      if (todo.checked) {
-        $todoItem.setAttribute("checked", "");
-      }
-
-      $todoItem.setAttribute("index", index);
-      $todoItem.addEventListener("onRemove", this._removeTodoItem.bind(this));
-
-      this.$todoList.appendChild($todoItem);
-    });
+    render(this.template(), this._shadowRoot, { eventContext: this });
   }
 }
 
